3dac349a6c1882bee4b70a6f471a1499a05bb49c
btom7447 <tombenjamin7447@gmail.com>
2025-09-20 10:47:55 +0100

    update to swagger config

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON
-app.use(cors());
+app.use(cors({
+  origin: "*",  
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"], // 🔑 important
+}));
 app.use(express.json());
 
 app.use(bodyParser.json());
